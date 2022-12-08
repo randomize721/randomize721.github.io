7d6418b40b704ff00a6d21c7388d97b6844613f3
Angga Darma Putra <anggadp91@gmail.com>
2022-12-08 14:37:12 +0800

    add defer to _document

--- pages/_document.js
+++ pages/_document.js
@@ -15,12 +15,13 @@ export default function Document() {
         <Main />
         <NextScript />
         <script
+          defer
           src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
           integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3"
           crossOrigin="anonymous"
         />
         
-        <script src="https://code.jquery.com/jquery-3.6.1.min.js" />
+        <script defer src="https://code.jquery.com/jquery-3.6.1.min.js" />
 
         <script
           defer
